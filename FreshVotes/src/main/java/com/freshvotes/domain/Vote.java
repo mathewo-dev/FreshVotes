3e4a84bd4237538ab1e3279018d2e4dbc271bff1
mathewo-dev <mathew.outing@gmail.com>
2020-05-04 14:51:13 +0200

    adding the first of the domain objects

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/Vote.java
@@ -0,0 +1,28 @@
+package com.freshvotes.domain;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class Vote {
+
+  private VoteId pk;
+  private boolean upvote;
+
+  public boolean isUpvote() {
+    return upvote;
+  }
+
+  public void setUpvote(boolean upvote) {
+    this.upvote = upvote;
+  }
+
+  @EmbeddedId
+  public VoteId getPk() {
+    return pk;
+  }
+
+  public void setPk(VoteId pk) {
+    this.pk = pk;
+  }
+}
