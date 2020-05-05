a3520fc43049748fd25ccfb6121efdfea5c83220
mathewo-dev <mathew.outing@gmail.com>
2020-05-05 15:05:06 +0200

    Finished security and implemented roles for authorization

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
@@ -1,15 +1,20 @@
 package com.freshvotes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private UserDetailsService userDetailsService;
 
   @Bean
   public PasswordEncoder getPasswordEncoder() {
@@ -18,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
 
 //    auth.inMemoryAuthentication().passwordEncoder(getPasswordEncoder()).withUser("mathewo")
 //    .password(getPasswordEncoder().encode("asdfasdf")).roles("USER");
@@ -27,9 +33,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/").permitAll().anyRequest().hasRole("USER").and().formLogin()
-        .loginPage("/login").permitAll().defaultSuccessUrl("/dashboard", true).and().logout().logoutUrl("/logout")
-        .permitAll();
+    http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/admin/**").hasRole("ADMIN").anyRequest()
+        .hasRole("USER").and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/dashboard", true).and()
+        .logout().logoutUrl("/logout").permitAll();
 
     // super.configure(http);
   }
